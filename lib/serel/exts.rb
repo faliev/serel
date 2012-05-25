b558b94d73b344e5fe2b8d711775c36ae90889d4
Farid Aliev <farid@entelo.com>
2012-05-25 13:33:38 -0700

    commented out singleton methods and class_attribute

--- lib/serel/exts.rb
+++ lib/serel/exts.rb
@@ -10,62 +10,62 @@ rescue NameError
   # Ignore this.
 end
 
-def singleton_class?
-  !name || '' == name
-end
-
-def singleton_class
-  class << self
-    self
-  end
-end
-
-def class_attribute(*attrs)
-  if attrs.last.is_a?(Hash)
-    options = attrs.pop
-  else
-    options = {}
-  end
-  instance_reader = options.fetch(:instance_reader, true)
-  instance_writer = options.fetch(:instance_writer, true)
-
-    attrs.each do |name|
-      class_eval <<-RUBY, __FILE__, __LINE__ + 1
-        def self.#{name}() nil end
-        def self.#{name}?() !!#{name} end
-
-        def self.#{name}=(val)
-          singleton_class.class_eval do
-            remove_possible_method(:#{name})
-            define_method(:#{name}) { val }
-          end
-
-          if singleton_class?
-            class_eval do
-              remove_possible_method(:#{name})
-              def #{name}
-                defined?(@#{name}) ? @#{name} : singleton_class.#{name}
-              end
-            end
-          end
-          val
-        end
-
-        if instance_reader
-          remove_possible_method :#{name}
-          def #{name}
-            defined?(@#{name}) ? @#{name} : self.class.#{name}
-          end
-
-          def #{name}?
-            !!#{name}
-          end
-        end
-      RUBY
-
-      attr_writer name if instance_writer
-  end
-end
+# def singleton_class?
+#   !name || '' == name
+# end
+# 
+# def singleton_class
+#   class << self
+#     self
+#   end
+# end
+# 
+# def class_attribute(*attrs)
+#   if attrs.last.is_a?(Hash)
+#     options = attrs.pop
+#   else
+#     options = {}
+#   end
+#   instance_reader = options.fetch(:instance_reader, true)
+#   instance_writer = options.fetch(:instance_writer, true)
+# 
+#     attrs.each do |name|
+#       class_eval <<-RUBY, __FILE__, __LINE__ + 1
+#         def self.#{name}() nil end
+#         def self.#{name}?() !!#{name} end
+# 
+#         def self.#{name}=(val)
+#           singleton_class.class_eval do
+#             remove_possible_method(:#{name})
+#             define_method(:#{name}) { val }
+#           end
+# 
+#           if singleton_class?
+#             class_eval do
+#               remove_possible_method(:#{name})
+#               def #{name}
+#                 defined?(@#{name}) ? @#{name} : singleton_class.#{name}
+#               end
+#             end
+#           end
+#           val
+#         end
+# 
+#         if instance_reader
+#           remove_possible_method :#{name}
+#           def #{name}
+#             defined?(@#{name}) ? @#{name} : self.class.#{name}
+#           end
+# 
+#           def #{name}?
+#             !!#{name}
+#           end
+#         end
+#       RUBY
+# 
+#       attr_writer name if instance_writer
+#   end
+# end
 
 def find_constant(symbol)
   str = symbol.to_s.dup
